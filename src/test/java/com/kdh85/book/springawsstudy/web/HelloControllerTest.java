9bb69a4e81cf7ebd11f3ea5255b3456098de045c
kdh85 <modudamba@naver.com>
2021-10-31 19:50:33 +0900

    feat : TDD로 단순한 컨트롤러 호출과 호출 Mock test 작성 * 단순 화면 호출 메소드 생성 및 MockMvc 테스트코드 작성. * 샘플 dto 작성. * 단순 파라메터들을 제공 받아 dto를 생성하여 리턴하는 호출 메소드 생성 및 MockMvc 테스트코드 작성.

---
+++ src/test/java/com/kdh85/book/springawsstudy/web/HelloControllerTest.java
@@ -0,0 +1,42 @@
+package com.kdh85.book.springawsstudy.web;
+
+import static org.hamcrest.Matchers.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+
+@ExtendWith(SpringExtension.class)
+@WebMvcTest
+class HelloControllerTest {
+
+	@Autowired
+	private MockMvc mockMvc;
+
+	@Test
+	void return_hello_test() throws Exception {
+		String hello = "hello";
+
+		mockMvc.perform(get("/hello"))
+			.andExpect(status().isOk())
+			.andExpect(content().string(hello));
+	}
+
+	@Test
+	void return_helloResponseDto_test() throws Exception {
+		String name = "hello";
+		int amount = 1000;
+
+		mockMvc.perform(get("/hello/dto")
+				.param("name", name)
+				.param("amount", String.valueOf(amount)))
+			.andExpect(status().isOk())
+			.andExpect(jsonPath("$.name", is(name)))
+			.andExpect(jsonPath("$.amount", is(amount)));
+	}
+}
