835be24a6021706ec15efe93d7c9f27cef1ce0d8
kdh85 <modudamba@naver.com>
2021-10-31 18:26:28 +0900

    Initial commit

---
+++ src/main/java/com/kdh85/book/springawsstudy/SpringAwsStudyApplication.java
@@ -0,0 +1,13 @@
+package com.kdh85.book.springawsstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAwsStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAwsStudyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kdh85/book/springawsstudy/SpringAwsStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kdh85.book.springawsstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAwsStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
